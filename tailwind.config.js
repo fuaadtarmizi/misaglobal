e0775badeed62b3736941723fb1fb9107216d64a
fuaadtarmizi <fuaad2120@gmail.com>
2024-08-19 11:36:30 +0800

    test1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,35 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    
     extend: {
+      keyframe:{
+        "accordion-down":{
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)"},
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)"},
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accoordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        times: ['"Arial"', 'serif'],
+        anton: ['Anton', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
